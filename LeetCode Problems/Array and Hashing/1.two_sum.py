f074629a00f99e93e815eee962614da2f8f238a3
Nikhil Omkar <61050785+foreigndemon@users.noreply.github.com>
2022-07-05 16:19:18 +0530

    Update 1.two_sum.py

--- LeetCode Problems/Array and Hashing/1.two_sum.py
+++ LeetCode Problems/Array and Hashing/1.two_sum.py
@@ -1,19 +1,8 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        hash_s, hash_t = {}, {}
-        for val in s:
-            if val not in hash_s:
-                hash_s[val] = 1
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for idx, val in enumerate(nums):
+            if val not in hash:
+                hash[target-val] = idx
             else:
-                hash_s[val] += 1                
-        for val in t:
-            if val not in hash_t:
-                hash_t[val] = 1
-            else:
-                hash_t[val] += 1
-                
-        if hash_s == hash_t:
-            return True
-        else:
-            False
-        
+                return [idx,hash[val]]
